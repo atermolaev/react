9032f9b4a16cda059d7438db594598e03a222f5f
Anton Termolaev <develop65@datum-group.ru>
2020-08-12 17:13:58 +0300

    Добавил загрузку данных

--- app/components/Home/index.jsx
+++ app/components/Home/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import 'babel-polyfill';
 
 class Home extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            data: ''
+        }
+    }
+
+    componentDidMount(){
+        this.getData();
+    }
+
+    getData = async () => {
+        let res = await axios.get('/data.json');
+        this.setState({data: JSON.stringify(res.data)});
+    }
+
     render(){
-        return (<div>Home</div>)
+        let { data } = this.state
+
+        return (
+            <div>
+                <div>Home</div>
+                <div>{data}</div>
+            </div>
+        )
     }
 }
 
