b69021e842c6f4ce7385b0720439b6d4e30bc1d3
Anton <driver_at@mail.ru>
2021-02-28 05:27:49 +0300

    Добавил material ui

--- app/components/Page1/index.jsx
+++ app/components/Page1/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Button } from '@material-ui/core'
 
 class Page1 extends React.Component {
+    constructor(){
+        super()
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick () {
+        alert('click');
+    }
+
     render(){
-        return (<div>Page1</div>)
+        return (<div style={{ marginLeft: '200px'}}>
+            <div>Page1</div>
+            <div><Button onClick={this.handleClick}>Запрос</Button></div>
+        </div>)
     }
 }
 
